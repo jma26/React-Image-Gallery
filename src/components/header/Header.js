b495cf4e2c42bdfc29fb65c223cf4a08fa98ae89
Jesse Ma <j.masonoma17@gmail.com>
2021-09-08 23:54:07 -0700

    WIP Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,60 @@
+import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
-import MenuIcon from "@material-ui/icons/Menu";
+import InstagramIcon from '@material-ui/icons/Instagram';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import ExploreIcon from '@material-ui/icons/Explore';
+import MessageIcon from '@material-ui/icons/Message';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import './header.css';
+
+const useStyles = makeStyles({
+  root: {
+    '& .header-middle .MuiIconButton-root': {
+      marginLeft: '0.5em',
+      marginRight: '0.5em'
+    }
+  }
+});
 
 function Header() {
+  const classes = useStyles();
   return (
-    <div className="Header">
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <MenuIcon />
+    <>
+      <AppBar className="header" position="static" color="transparent">
+        <Toolbar className={classes.root}>
+          <IconButton
+            aria-label="Home page"
+            color="inherit"
+            onClick={() => window.location.replace('/home')}
+          >
+            <InstagramIcon />
           </IconButton>
           <Typography variant="h6">
             Reactagram
           </Typography>
+          <div className="header-middle mid-icons">
+            <IconButton aria-label="Home page" color="inherit">
+              <HomeIcon />
+            </IconButton>
+            <IconButton aria-label="Search function" color="inherit">
+              <SearchIcon />
+            </IconButton>
+            <IconButton aria-label="Explore Reactagram" color="inherit">
+              <ExploreIcon />
+            </IconButton>
+          </div>
+          <div className="header-end account">
+            <IconButton aria-label="User messages" color="inherit">
+              <MessageIcon />
+            </IconButton>
+            <IconButton aria-label="User account" color="inherit">
+              <AccountCircleIcon />
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
-    </div>
+    </>
   );
 }
 
